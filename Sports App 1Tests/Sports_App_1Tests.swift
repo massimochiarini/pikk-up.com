4d12736dbec8e58cba8d706d7122a3f4f7bdb7c9
Massimo Chiarini <massimochiarini@outlook.com>
2025-12-15 17:24:03 -0500

    Initial Commit

---
+++ Sports App 1Tests/Sports_App_1Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Sports_App_1Tests.swift
+//  Sports App 1Tests
+//
+//  Created by Massimo Chiarini on 12/15/25.
+//
+
+import Testing
+
+struct Sports_App_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
