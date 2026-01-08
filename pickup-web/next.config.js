0f3a16a2bc1cc3083acf07cbfadffb0272b5bc03
massimochiarini <44180379+massimochiarini@users.noreply.github.com>
2026-01-08 16:47:01 -0500

    Fix: properly ignore supabase folder in next.config.js

--- pickup-web/next.config.js
+++ pickup-web/next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ['your-supabase-project.supabase.co'],
   },
+  webpack: (config, { isServer }) => {
+    // Ignore supabase functions folder (Deno edge functions)
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/supabase/**', '**/node_modules/**'],
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
